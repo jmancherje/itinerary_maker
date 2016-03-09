dfc5df325e6a16726c526f1d6cf62ae4d831963d
Justin Mancherje <mancherje.justin@gmail.com>
2016-03-08 21:03:51 -0800

    render main app component

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
+import App from './components/App'
 
 ReactDOM.render(
-  <h2>Hi</h2>,
+  <App />,
   document.querySelector('#root')
 )
