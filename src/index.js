c3523a52628874aafe0ebf2eded645bd88181264
Justin Mancherje <mancherje.justin@gmail.com>
2016-03-08 17:54:42 -0800

    initial react app rendering

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+
+ReactDOM.render(
+  <h2>Hi</h2>,
+  document.querySelector('#root')
+)
