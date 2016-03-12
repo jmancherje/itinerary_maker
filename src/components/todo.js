da1104e8104163d5fc6c36faf1eb1db40bbd73db
Justin Mancherje <mancherje.justin@gmail.com>
2016-03-11 21:21:15 -0800

    add todo component

---
+++ src/components/todo.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+
+let nextTodoId = 0;
+
+class TodoApp extends Component {
+  render() {
+    return (
+      <div>
+        <input type="text" ref="todoInput" />
+        <button onClick={() => {
+          store.dispatch({
+            type: 'ADD_TODO',
+            text: this.refs.todoInput.value,
+            id: nextTodoId++
+          });
+          this.refs.todoInput.value = '';
+        }}>
+          Add Todo
+        </button>
+        <ul>
+          {this.props.todos.map(todo => 
+              <li key={todo.id}>
+                {todo.text}
+              </li>
+            )}
+        </ul>
+      </div>
+    )
+  }
+}
+
+export default TodoApp
