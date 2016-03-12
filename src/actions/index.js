87eb4c5065fb5f40b0a0bb1c97dbe7af9e704568
Justin Mancherje <mancherje.justin@gmail.com>
2016-03-11 22:22:30 -0800

    add action creater for visibilityFilter

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,8 +3,8 @@ export const DECREMENT = 'DECREMENT'
 export const ADD_TODO = 'ADD_TODO'
 export const TOGGLE_TODO = 'TOGGLE_TODO'
 
+// increments the id for unique ids
 let nextTodoId = 0
-
 export const addTodo = (text) => {
   return {
     type: ADD_TODO,
@@ -20,39 +20,9 @@ export const toggleTodo = (id) => {
   }
 }
 
-// // const FETCH_PEOPLE_FAILURE = 'FETCH_PEOPLE_FAILURE'
-
-// export function increment() {
-//   return {
-//     type: INCREMENT
-//   }
-// }
-
-// export function decrement() {
-//   return {
-//     type: DECREMENT
-//   }
-// }
-
-
-// //////////////////////////////////////////////////////////
-
-// export function fetchPeople() {
-//   return {
-//     type: FETCH_PEOPLE
-//   }
-// }
-
-// export function receivePeople(data) {
-//   return {
-//     type: RECEIVE_PEOPLE,
-//     people: data,
-//     receivedAt: Date.now()
-//   }
-// }
-
-// export function increment(data) {
-//   return {
-//     type: INCREMENT
-//   }
-// }
+export const setVisibilityFilter = (filter) => {
+  return {
+    type: 'SET_VISIBILITY_FILTER',
+    filter
+  }
+}
