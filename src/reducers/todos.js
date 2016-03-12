713cd04a1c0df7d451ba232a9f4866303ea15933
Justin Mancherje <mancherje.justin@gmail.com>
2016-03-11 21:28:21 -0800

    create todo reducer functions

---
+++ src/reducers/todos.js
@@ -0,0 +1,44 @@
+import { ADD_TODO, TOGGLE_TODO } from '.../actions'
+
+const todo = (state, action) => {
+  switch (action.type) {
+    case ADD_TODO:
+      return [
+        ...state,
+        {
+          id: action.id,
+          text: actions.text,
+          completed: false
+        }
+      ]
+    case TOGGLE_TODO:
+      if (state.id !== action.id) {
+        return state
+      }
+
+      return Object.assign({}, state {
+        completed: !state.completed
+      })
+
+    default:
+      return state;
+  }
+};
+
+const todos = (state = [], action) => {
+  switch (action.type) {
+    case ADD_TODO:
+      return [
+        ...state,
+        todo(undefined, action)
+      ]
+    case TOGGLE_TODO:
+      return state.map(t => 
+        todo(t, action)
+      )
+    default:
+      return state
+  }
+}
+
+export default todos
