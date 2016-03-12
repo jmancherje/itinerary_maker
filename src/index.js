d5c864354881d03d2f35133395924ac2a3a2b48a
Justin Mancherje <mancherje.justin@gmail.com>
2016-03-11 22:31:24 -0800

    refactor to use ReactDOM.render and remove unnecessary components

--- src/index.js
+++ src/index.js
@@ -1,36 +1,16 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import reactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
 import App from './components/App'
 import reducer from './reducers'
 import Counter from './components/counter'
 
-const store = createStore(reducer)
+let store = createStore(reducer)
 
-const render = () => {
-  ReactDOM.render(
-    <Provider store={store}>
-      <App />
-    </Provider>,
-    document.getElementById('root')
-  )
-}
-
-store.subscribe(render)
-render()
-
-
-// toggle todo function:
-// const toggleTodo = (todo) => {
-//   return Object.assign({}, todo, {
-//     completed: !todo.completed
-//   });
-// };
-
-// const toggleTodoWithSpread = (todo) => {
-//   return {
-//     ...todo,
-//     completed: !todo.completed
-//   };
-// };
+reactDOM.render(
+  <Provider store={store} >
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
